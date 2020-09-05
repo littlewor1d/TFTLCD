858801fd04b815599c71c461f072a5011381f8cc
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-05 19:29:25 +0000

    Added translation using Weblate (Hebrew)

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -19,4 +19,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationFor
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSessio
