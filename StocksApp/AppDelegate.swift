9e6a9ff74f09cfc8f3de746436d98a42673912ef
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-06 21:04:51 +0000

    -u

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -20,4 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-   
+        // Called when a new scene session is being created
