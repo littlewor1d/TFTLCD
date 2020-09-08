aaa48c9e502ed158e17dc238ea79be73d9d4e50b
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-08 20:09:49 +0000

    Bump addressable from 2.7.0 to 2.8.0

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -20,4 +20,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created
+        // Called when a new scene session is being created.
+        // Use this method to select a configuratio
