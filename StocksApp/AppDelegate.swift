37aa4efef6498966bd39ffa9e66760481bb19f8d
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-09 09:55:23 +0000

    get tests passing

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -22,4 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISce
+        return UISceneConfiguration(name: "Default Configuration", s
