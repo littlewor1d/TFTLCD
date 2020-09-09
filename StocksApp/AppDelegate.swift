7ff121ca64b3d6020a7d5eaf5b27a1d8c87e4d86
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-09 06:37:07 +0000

    Version 0.1.1.

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -21,4 +21,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
-        // Use this method to select a configuratio
+        // Use this method to select a configuration to create the new scene with.
+        return UISce
