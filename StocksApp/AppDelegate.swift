fe4ff02ec9c157b9c1d511788c2a38be302f0937
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-15 10:56:13 +0000

    staging: comedi: prevent auto-unconfig of manually configured devices

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -25,4 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication,
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession
