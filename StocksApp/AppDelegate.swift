ce8eebc0c1b55266908497b25b052c0daee1ca9a
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2020-09-19 13:38:47 +0000

    Minor change in README

--- StocksApp/AppDelegate.swift
+++ StocksApp/AppDelegate.swift
@@ -28,4 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this me
+        // Use this method to release any resources that were specific to the
