4848c347ede4b5c1072d9081230311aa567f2ed5
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-03-01 15:10:38 +0000

    docstrings

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -2,4 +2,9 @@
 //  SceneDelegate.swift
 //  StocksApp
 //
-//  Created by Alexey Grebennikov on 2.05.
+//  Created by Alexey Grebennikov on 2.05.21.
+//
+
+import UIKit
+
+class SceneDelegate: UIRespon
