defd806729351ba434f5fbf586897d4994b2dc8a
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-03-19 20:43:18 +0000

    Next try

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -20,4 +20,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as 
+        // Called as the scene is being released by the system.
