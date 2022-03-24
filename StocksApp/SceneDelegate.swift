8147e90f5a12fbb6f38273c7e733daecac0cf1d2
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-03-24 15:26:44 +0000

    staging: android: ashmem: Avoid deadlock with mmap/shrink

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -23,4 +23,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // 
+        // The scene may re-connect later, as its session was not 
