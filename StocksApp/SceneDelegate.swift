97a595f2a724cee1fa7c18f0f2ad079f619563a4
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-03-26 09:42:02 +0000

    premerge

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -27,4 +27,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active st
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restar
