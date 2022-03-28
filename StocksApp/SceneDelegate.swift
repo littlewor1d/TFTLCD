97b45fe5cc98d65e2f4ee9eabae27097a53bfb83
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-03-28 06:43:23 +0000

    Redefine ATOMIC_INIT and ATOMIC64_INIT to drop the casts

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -28,4 +28,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restar
+        // Use this method to restart any tasks that were paused (or not yet st
