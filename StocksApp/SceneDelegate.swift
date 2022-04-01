3bd28be6aeb4753cc8b833eff3951b9006ef6f8b
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-04-01 11:35:21 +0000

    Added travis configuration.

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -32,4 +32,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-      
+        // Called when the scene will move from an active s
