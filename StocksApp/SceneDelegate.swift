004edf01f15b68d3af1b61809934ffc3c76b708d
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-04-09 13:41:09 +0000

    mv

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -38,3 +38,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on 
