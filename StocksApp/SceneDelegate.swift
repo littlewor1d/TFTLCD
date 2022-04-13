8e8765228b556ee6d17e88db91506f1a775dec12
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-04-13 17:00:27 +0000

    rm debug code

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -43,4 +43,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
-  
+        // Use this method to save data, release shared resources, and store
