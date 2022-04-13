b5be1add00fda9d07747d9fdaa9721b719f415ce
littlewor1d <112325858+littlewor1d@users.noreply.github.com>
2022-04-13 11:07:02 +0000

    rebuilding site

--- StocksApp/SceneDelegate.swift
+++ StocksApp/SceneDelegate.swift
@@ -42,4 +42,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene 
+        // Called as the scene transitions from the foreground to the background.
+  
